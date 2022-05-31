81f7f6a40490e6319ac9e2f108ab26e9ba8074a4
naresh <nareshv635@gmail.com>
2022-05-31 19:16:43 +0530

    first commit

--- my-app/src/header/header.js
+++ my-app/src/header/header.js
@@ -1,12 +1,29 @@
 import React from "react"
 import './header.css';
 
-function Header(){
-    return(
-        <div>
-            <h1>hello</h1>
+function Header() {
+    return (
+        <div className="header-main">
+            <div className="title">
+                <h1>Resume</h1>
+            </div>
+
+            <div className="link">
+                <ul className="list">
+                    <li className="list-link">about me</li>
+                    <li className="list-link">skills</li>
+                    <li className="list-link">project</li>
+                    <li className="list-link">contact me</li>
+
+                </ul>
+            </div>
+
+            <div className="empty">
+         
+            </div>
+
         </div>
-    );;
+    );
 }
 
 export default Header;
